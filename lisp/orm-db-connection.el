c38f424942387377b3e6344c00b2cc0b6888071f
Panayotis Manganaris <panos.manganaris@gmail.com>
2024-08-11 20:47:55 -0400

    connection [FIX] default .db location

--- lisp/orm-db-connection.el
+++ lisp/orm-db-connection.el
@@ -14,7 +14,7 @@
     (:sql (emacsql-sqlite-builtin (oref db file)))
     (_ nil)))
 
-(defvar orm-default-db (orm-db :type :sql :file "/home/tassos/desktop/test.db"))
+(defvar orm-default-db (orm-db :type :sql :file (expand-file-name "~/test.db")))
 (defvar orm-default-conn (orm-connect orm-default-db))
 
 (provide 'orm-db-connection)
