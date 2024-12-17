c8ad61697a36da781dc87e82583d1dde4fca5334
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-12-16 23:03:47 -0500

    Add read functions orm-all and orm-first

--- lisp/orm.el
+++ lisp/orm.el
@@ -18,6 +18,9 @@
   "Get table column names"
   (orm-column-names (make-instance table)))
 
+(defun symbol-to-keyword (sym)
+  (intern-soft (s-prepend ":" (symbol-name sym))))
+
 ;; Static Methods
 
 (cl-defmethod orm-count ((table (subclass orm-table)))
@@ -46,7 +49,9 @@
 				(slot-value this slot)
 			      nil)))))
 
-;; Insert
+;; CRUD
+
+;; Create - orm-insert
 
 (cl-defmethod orm-insert ((this orm-table))
   "Insert object into database."
@@ -56,13 +61,30 @@
     (emacsql-with-transaction conn
       (emacsql conn [:insert :into $i1 :values $v2] table-name values))))
 
-;; (cl-defmethod orm-replace ((this orm-table))
-;;   "Insert object into database."
-;;   (let ((conn orm-default-conn)
-;; 	(table-name (orm-table-name this))
-;; 	(values (orm-object-values this)))
-;;     (emacsql-with-transaction conn
-;;       (emacsql conn [:insert :into $i1 :values $v2] table-name values))))
+;; Read - orm-all, orm-first
 
+(cl-defmethod orm--make-from-record ((table (subclass orm-table)) record)
+  (apply 'make-instance (cons table (-interleave (-map 'symbol-to-keyword (orm-column-names table)) record))))
+
+(cl-defmethod orm-all ((table (subclass orm-table)))
+  "Select from table for class in database."
+  (let* ((conn orm-default-conn)
+	 (table-name (orm-table-name table))
+	 (records (emacsql-with-transaction conn
+		    (emacsql conn [:select :* :from $S1] (vector table-name)))))
+    (mapcar (lambda (r) (orm--make-from-record table r)) records)))
+
+(cl-defmethod orm-first ((table (subclass orm-table)))
+  "Select from table for class in database."
+  (let* ((conn orm-default-conn)
+	 (table-name (orm-table-name table))
+	 (record (emacsql-with-transaction conn
+		   (emacsql conn [:select :* :from $S1 :asc :limit $s2] (vector table-name) 1))))
+    (when record
+      (orm--make-from-record table (car record)))))
+
+
+;; Update - orm-update
+;; Delete - orm-delete
 
 (provide 'orm)
